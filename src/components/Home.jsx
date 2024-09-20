99a06ce98a920edcc2c80d41217fb6499b6392b7
Daniel Fletcher <fletcherdaniel612@gmail.com>
2024-09-20 15:36:05 -0500

    fixed bg color bug

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 
 const Home = () => (
-<div>
-    <h1 className="text-center">Home Page</h1>
+    <div>
+        <h1 className="text-center">Home Page</h1>
 
-    <div class = 'container'>
-    <h1>Welcome to the website's Home Page!
-    <br></br>
-    Edit main.jsx to edit HTML on the home page
-    <br></br>
-    Edit index.css to style this
-    <br></br>
-    Base Root is in SRC
-    </h1>
+        <div class="container">
+            <h1>
+                Welcome to the website's Home Page!
+                <br></br>
+                Edit main.jsx to edit HTML on the home page
+                <br></br>
+                Edit index.css to style this
+                <br></br>
+                Base Root is in SRC
+            </h1>
+        </div>
     </div>
-
-</div>
-
 );
 
 export default Home;
